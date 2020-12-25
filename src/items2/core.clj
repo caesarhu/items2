d259b66d04a868f0eb2a2cee7f8a899fa04ee90f
caesarhu <hupeishun@gmail.com>
2020-12-25 16:52:11 +0800

    fix run time error

--- src/items2/core.clj
+++ src/items2/core.clj
@@ -4,12 +4,11 @@
             [items2.config :as config]
             [items2.db.mail :as mail]))
 
-(clip/set-init! (fn [] @config/config))
-(clip/start)
-
 (defn send-mail
   "每日寄送危安物品登錄APP mail"
   [& args]
+  (clip/set-init! (fn [] @config/config))
+  (clip/start)
   (println "開始寄送危安物品登錄APP mail !")
   (mail/daily-mail)
   (println "結束寄送危安物品登錄APP mail !")
