aca3709dbb3c1a687cb9d12db2f911672a8e9a1f
caesarhu <hupeishun@gmail.com>
2020-12-25 13:38:12 +0800

    testing send-mail

--- src/items2/core.clj
+++ src/items2/core.clj
@@ -1,8 +1,9 @@
 (ns items2.core
   (:gen-class)
-  (:require [juxt.clip.repl :refer [start stop set-init! reset system] :as clip]
+  (:require [juxt.clip.repl :as clip]
             [items2.config :as config]
-            [items2.db.items :as items]
+            [datoteka.core :as fs]
+            [jsonista.core :as json]
             [items2.db.mail :as mail]))
 
 (defn send-mail
@@ -12,7 +13,18 @@
   (clip/start)
   (println "開始寄送危安物品登錄APP mail !")
   (mail/daily-mail)
-  (println "結束寄送危安物品登錄APP mail !"))
+  (println "結束寄送危安物品登錄APP mail !")
+  (clip/stop))
+
+(defn test-read-file
+  "每日寄送危安物品登錄APP mail"
+  [& args]
+  (let [json-file "/home/shun/sftp/upload/2020-12-25-07-43-10.581-DataStore.json"
+        file (fs/file json-file)]
+    (clip/set-init! (fn [] @config/config))
+    (clip/start)
+    (println (json/read-value file))
+    (clip/stop)))
 
 (defn -main
   "每日寄送危安物品登錄APP mail"
